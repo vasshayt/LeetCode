6e0ec5ae888f4f6e6c1d76d21b59fd7e27bc667c
vasshayt <vasshayt@gmail.com>
2021-04-30 17:51:18 +0300

    Add repositories

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions are from Leet code
---
+++ src/main/java/problems/n1_n100/n1/Solution.java
@@ -0,0 +1,15 @@
+package problems.n1_n100.n1;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 6;
+        new Solution().twoSum(nums, target);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+
+        return res;
+    }
+}
