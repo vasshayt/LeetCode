95edf3b24062406c69142c9134aea3da5dfb3394
vasshayt <vasshayt@gmail.com>
2021-05-05 09:50:38 +0300

    add Add Two Numbers

---
+++ src/main/java/problems/p1_p99/p2/ListNode.java
@@ -0,0 +1,18 @@
+package problems.p1_p99.p2;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/problems/p1_p99/p2/Solution.java
@@ -0,0 +1,13 @@
+package problems.p1_p99.p2;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode();
+        ListNode list2 = new ListNode();
+        System.out.println(new Solution().addTwoNumbers(list1, list2));
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new ListNode();
+    }
+}
